0f1e38b84937dc361cd976965be4d18a2d5fb231
mkjmdski <mikolaj.mlodzikowski@gmail.com>
2017-11-25 22:39:38 +0100

    updated script for imagemagick

--- build-process/install-imagemagick.sh
+++ build-process/install-imagemagick.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+sudo apt-get install -y libjpeg-62 libjpeg62-dev
 wget ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick.tar.gz
-tar xvfz ImageMagick.tar.gz
+tar xfz ImageMagick.tar.gz
 cd ImageMagick-*
 ./configure --disable-shared
 make
 sudo make install
+cd ..
 rm -rf ImageMagick*
