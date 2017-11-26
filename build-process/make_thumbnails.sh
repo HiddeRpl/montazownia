a710a9bdc8ef1e1361ff433146840cf3da7d5402
mkjmdski <mikolaj.mlodzikowski@gmail.com>
2017-11-26 16:46:58 +0100

    img process fix

--- build-process/make_thumbnails.sh
+++ build-process/make_thumbnails.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-cd ../site/source/images/portfolio/img
+cd $1
 for filename in *.jpg; do
 	convert ${filename} -resize 256x256 ../thumbnails/${filename}
 done
-echo "files:" > a.yaml
-ls -l | awk '{print $NF}' | sed -e 's/^/  - /' | tail -n +3 >> a.yaml
-mv a.yaml ../../../../data/img.yaml
-rm -rf *.yaml
