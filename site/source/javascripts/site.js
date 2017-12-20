ee95f03619f327a62f11d75941474f9bfd2ba5df
mjarmula <mikolaj_jarmula@wp.pl>
2017-12-20 22:46:42 +0100

    Navbar proposition

--- site/source/javascripts/site.js
+++ site/source/javascripts/site.js
@@ -5,23 +5,27 @@ $(document).ready(function () {
     /*
         ### NAVBAR TRANSPARENCY ###
     */
-    function navbarScroll() {
-        var height = $(window).scrollTop();
-        var urlArray = location.pathname.split('/');
+    // function navbarScroll() {
+    //     var height = $(window).scrollTop();
+    //
+    //     if (height > 100) {
+    //         $('.navbarTrans').fadeIn('fast');
+    //     }
+    //
+    //     if (height === 0) {
+    //         $('.navbarTrans').fadeOut('fast');
+    //     }
+    //
+    // }
+    var urlArray = location.pathname.split('/');
 
-        if (urlArray[2] !== "") {
-            $('.navbarTrans').show();
-        } else {
-            if (height > 100) {
-                $('.navbarTrans').fadeIn('fast');
-            }
-
-            if (height === 0) {
-                $('.navbarTrans').fadeOut('fast');
-            }
-        }
+    if (urlArray[2] === undefined) {
+        setTimeout(function () {
+            $('.navbarTrans').fadeIn();
+        }, 1200)
+    } else {
+        $('.navbarTrans').show();
     }
-    navbarScroll();
 
     $(window).scroll(function () {
         navbarScroll();
